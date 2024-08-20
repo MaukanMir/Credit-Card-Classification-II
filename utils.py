89ad4f461f455c7a9f9c3d45ce7d5002c822535a
maukan <maukanm22@gmail.com>
2024-08-19 20:50:16 -0400

    added new models

--- utils.py
+++ utils.py
@@ -73,3 +73,131 @@ def sampling_pipeline(model, sampling_pipeline, kbest=None):
   return imb_pipeline(
     steps=steps
   )
+
+def pca_analysis(X):
+  
+  features = range(1, X.shape[1]+1)
+  metric_tracker = []
+
+  for n_component in features:
+    pca = PCA(n_components=n_component)
+    pipeline = create_sklearn_pipeline(pca)
+    X_pca = pipeline.fit_transform(X)
+    cumsum = np.sum(pca.explained_variance_ratio_)
+    
+    
+    metric_tracker.append(
+      {
+        "Component":n_component,
+        "CumSum": cumsum
+      }
+    )
+    
+    df = pd.DataFrame(metric_tracker)
+    
+    plt.figure(figsize=(20, 12))
+    plt.title("PCA Analysis")
+    sns.barplot(data=df, x="Component", y="CumSum")
+    plt.xlabel("Components")
+    plt.ylabel("CumSum")
+    plt.show()
+    
+    return df
+
+
+def plot_dataset(df, visual_type):
+  
+  plt.figure(figsize=(16,16))
+  cols = len(df.columns)
+  rows = (cols +3)//4
+  
+  for idx, col in enumerate(df.columns):
+    plt.subplot(rows, 4, idx+1)
+    plt.title(col)
+    sns.histplot(data=df[col], kde=True) if visual_type == "hist" else sns.boxplot(df[col])
+    plt.xlabel(col)
+  
+  plt.tight_layout()
+  plt.show()
+
+def aggregate_dataset(df, interested_columns, agg_col, function):
+  
+  for col in interested_columns:
+    grouped_df = getattr(df.groupby(agg_col)[col], function)().reset_index(name=col)
+    plt.figure(figsize=(12, 8))
+    sns.barplot(data=grouped_df, x=agg_col, y=col)
+    plt.title(f"Agg of {col}")
+    plt.ylabel(col)
+    plt.xticks(rotation =45)
+    plt.show()
+
+def plot_confusion_matrix(y_true, y_pred, model_name):
+  
+  plt.figure(figsize=(10, 7))
+  cm = confusion_matrix(y_true, y_pred)
+  sns.heatmap(data=cm, annot=True, fmt="d", cmap="Blues")
+  plt.title(f"Confusion Matrix For: {model_name}")
+  plt.ylabel("Predicted Labels")
+  plt.xlabel("True Labels")
+  plt.show()
+
+def grab_selected_models(names):
+  
+  models = {
+    "SVC": SVC(),
+    "LR":LogisticRegression(max_iter=1000),
+    "KNN": KNeighborsClassifier(),
+    "DTC": DecisionTreeClassifier(),
+    "GBC":GradientBoostingClassifier(),
+    "RFC":RandomForestClassifier(),
+    "XGB": XGBClassifier(),
+    "DUMMY": DummyClassifier(strategy="uniform")
+  }
+  
+  return [models[model_name] for model_name in names]
+
+def get_metrics(y, predictions):
+    acc_metric = accuracy_score(y, predictions)
+    recall_metric = recall_score(y, predictions, average='macro')
+    precision_metric = precision_score(y, predictions, average='macro')
+    f1_metric = f1_score(y, predictions, average='macro')
+    
+    return [acc_metric,recall_metric, precision_metric, f1_metric]
+
+def evaluate_model(model, X, y, metric):
+  
+  cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=10,random_state=1)
+  scores = cross_val_score(model, X, y, cv=cv, scoring=metric, n_jobs=-1)
+  preds = cross_val_predict(model, X, y, n_jobs=-1, cv=10)
+  acc, recall_metric, precision_metric, f1_metric = get_metrics(y, preds)
+  
+  return [
+    round(np.mean(scores),3), 
+    round(np.var(scores), 3), 
+    round(np.std(scores),3), 
+    round(acc,3), 
+    round(recall_metric,3),
+    round(precision_metric,3),
+    round(f1_metric,3)
+  ]
+
+def test_selected_models(model_names, models, X, y, scoring_metric, kbest=None):
+  
+  metric_tracker = []
+  
+  for model, model_name in zip(models, model_names):
+    pipeline = create_sklearn_pipeline(preprocessor, model, kbest) if kbest else create_sklearn_pipeline(preprocessor, model) 
+    scores = evaluate_model(pipeline, X, y, scoring_metric)
+    metric_tracker.append({
+      "Model": model_name,
+      "Mean": scores[0],
+      "Var": scores[1],
+      "STD": scores[2],
+      "Test-Acc":scores[3],
+      "recall-Score":scores[4],
+      "precision-Score":scores[5],
+      "F1-Score":scores[6]
+    })
+  
+  performance_df = pd.DataFrame(metric_tracker).sort_values(by="Mean", ascending=False)
+  return performance_df
