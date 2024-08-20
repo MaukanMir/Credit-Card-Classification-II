d602f605acae76164670ef17043da30cf31dac3f
maukan <maukanm22@gmail.com>
2024-08-19 20:32:05 -0400

    added new models

---
+++ utils.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, f_oneway
+
+# Preprocessing 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder, PowerTransformer
+
+# Model Selection and Metrics
+from sklearn.model_selection import RepeatedStratifiedKFold, RandomizedSearchCV, GridSearchCV, cross_val_predict, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
+from sklearn.model_selection import train_test_split
+
+
+from imblearn.pipeline import Pipeline as imb_pipeline
+from imblearn.over_sampling import SMOTE, SMOTEN, RandomOverSampler, BorderlineSMOTE
+
+# Models
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.dummy import DummyClassifier
+
+def check_nulls_dupes(df):
+  print(f"The Amount of Null Values: {df.isna().sum()}")
+  print(f"The Amount of Duplicated Values: {df.duplicated().sum()}")
+
+def check_class_imbalance(target):
+  
+  unique, counts = np.unique(target, return_counts=True)
+  plt.figure(figsize=(10, 8))
+  plt.pie(counts, labels=unique, startangle=140, autopct="%1.1f%%")
+  plt.title("Target Class Breakdown")
+  plt.legend()
+  plt.show()
+  
+  plt.figure(figsize=(10, 8))
+  sns.countplot(x=target)
+  plt.xlabel("Target Classes")
+  plt.ylabel("Counts")
+  plt.show()
